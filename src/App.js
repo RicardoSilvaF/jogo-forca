cb3e7c76c555ec51c7c660759179888473a1ac6a
RicardoSilvaF <zanphs@gmail.com>
2022-12-05 06:48:22 -0300

    Randomizar palavra e colocar os underlines

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import Chute from "./components/Chute"
 import Letras from "./components/Letras"
 import { useState } from "react";
 import alfabeto from "./alfabeto";
+import palavas from "./palavras"
+import palavras from "./palavras";
 
 export default function App() {
   const [desabilitarInput, setDesabilitarInput] = useState(true)
   const [letrasUsadas, setLetrasUsadas] = useState(alfabeto)
+  const [erros, setErros] = useState(0)
+  const [palavraJogo, setPalavraJogo] = useState([])
+  const [palavraEscolhida, setPalavraEscolhida] = useState([])
+
+  function iniciarJogo(){
+    sortearPalavra()
+    setDesabilitarInput(false);
+    setLetrasUsadas([])
+  }
+
+  function sortearPalavra(){
+    const i = Math.floor(Math.random() * palavras.length)
+    const palavra = palavras[i]
+    const palavraArray = palavra.split("")
+    setPalavraEscolhida(palavraArray)
+    let underline = []
+    palavraArray.forEach(() => underline.push(" _"))
+    setPalavraJogo(underline)
+  }
 
   return (
     <div className="App">
-      <Jogo/>
+      <Jogo iniciarJogo={iniciarJogo} erros={erros} palavraJogo={palavraJogo}/>
       <Letras letrasUsadas={letrasUsadas}/>
       <Chute desabilitarInput={desabilitarInput}/>
     </div>
--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,11 +1,20 @@
 import forca0 from "../assets/forca0.png"
+import forca1 from "../assets/forca1.png"
+import forca2 from "../assets/forca2.png"
+import forca3 from "../assets/forca3.png"
+import forca4 from "../assets/forca4.png"
+import forca5 from "../assets/forca5.png"
+import forca6 from "../assets/forca6.png"
 
-export default function Jogo(){
+export default function Jogo(props){
+    const {iniciarJogo, erros, palavraJogo} = props;
+    const imagens = [forca0,forca1,forca2,forca3,forca4,forca5,forca6]
+
     return(
         <div className="area-geral">
-            <img className="imagem-forca" src={forca0} alt="forca"/>
-            <button className="botao-escolher-palavra">Escolher palavra</button>
-            <h1 className="palavra-secreta">u _ a _ _ _ _</h1>
+            <img className="imagem-forca" src={imagens[erros]} alt="forca"/>
+            <button className="botao-escolher-palavra" onClick={iniciarJogo}>Escolher palavra</button>
+            <h1 className="palavra-secreta">{palavraJogo}</h1>
         </div>
     )
 }
